a6c941978563ee6d1076871908d0d629602d05ca
Robert Stumborg <rob@stumb.org>
2026-03-25 18:35:18 -0500

    neovim: fzflua customizations

--- .config/nvim/lua/fzf.lua
+++ .config/nvim/lua/fzf.lua
@@ -1,5 +1,27 @@
--- file picker
 vim.pack.add({"https://github.com/ibhagwan/fzf-lua"})
+
+-- avoids being slow to close
+vim.opt.ttimeoutlen = 0
 
 vim.keymap.set("n", "<c-p>", ":FzfLua files<cr>")
 vim.keymap.set("n", "<c-f>", ":FzfLua live_grep<cr>")
+vim.keymap.set("n", "<c-h>", ":FzfLua buffers<cr>")
+
+local actions = require('fzf-lua.actions')
+require('fzf-lua').setup({
+  winopts = { preview = { title = true }},
+  keymap = {
+    builtin = {
+      ["<C-d>"] = "preview-page-down",
+      ["<C-u>"] = "preview-page-up",
+      ["<C-p>"] = "toggle-preview",
+    },
+  },
+  actions = {
+    files = {
+      ["ctrl-n"] = actions.toggle_ignore,
+      ["ctrl-h"] = actions.toggle_hidden,
+      ["enter"]  = actions.file_edit_or_qf,
+    }
+  },
+})
