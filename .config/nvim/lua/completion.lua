48db409cfff8d8482716971cc2da83a0608c08ed
Robert Stumborg <rob@stumb.org>
2026-03-26 15:57:06 -0500

    neovim: change some blink.cmp cmd completion options

--- .config/nvim/lua/completion.lua
+++ .config/nvim/lua/completion.lua
@@ -56,11 +56,26 @@ require('blink.cmp').setup({
 
   cmdline = {
     keymap = {
-      preset = 'inherit',
+      ['<Tab>'] = { 'accept' },
       ['<CR>'] = { 'accept_and_enter', 'fallback' },
     },
+    -- (optionally) automatically show the menu
+    completion = { menu = { auto_show = true } }
   },
 
-  sources = { default = { "lsp" } }
+  sources = {
+    default = {
+      "lsp"
+    },
+    providers = {
+      cmdline = {
+        min_keyword_length = function(ctx)
+          -- when typing a command, only show when the keyword is 3 characters or longer
+          if ctx.mode == 'cmdline' and string.find(ctx.line, ' ') == nil then return 3 end
+          return 0
+        end
+      }
+    }
+  }
 })
 
