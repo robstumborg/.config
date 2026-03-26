7d5483ec376c22c2173428dcf54e7fec545fd7df
Robert Stumborg <rob@stumb.org>
2026-03-25 19:24:20 -0500

    neovim: add lsp keybinds

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -3,3 +3,15 @@ vim.lsp.enable({
 })
 
 vim.diagnostic.config({ virtual_text = false })
+
+-- lsp keybinds
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+  callback = function(ev)
+    local opts = { buffer = ev.buf }
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+    vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
+    vim.keymap.set('n', '<leader>cr', vim.lsp.buf.rename, opts)
+  end,
+})
